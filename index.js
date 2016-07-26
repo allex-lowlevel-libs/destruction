384cb207c699bae0778887591728fd712e800e86
Luka Petrovic <lukapetrovicsi@gmail.com>
2016-07-26 11:28:58 +0200

    Added checking functions. Usage of the lib is changed: it exports a function instead of an object. Every function returns true if it is successfully executed. Fully tested.

--- index.js
+++ index.js
@@ -1,59 +1,131 @@
-'use strict';
-function arryDestroyEl (el, index, arr) {
-  if (el) el.destroy();
-  arr[index] = null;
-}
+function createLib(has,isFunction,isArray,isNumber,isString){
+  'use strict';
 
-function arryNullEl (el, index, arr) {
-  if (!arr) return;
-  arr[index] = null;
-}
-
-function arryDestroyAll (arr) {
-  if (!arr) return;
-  arr.forEach(arryDestroyEl);
-}
-
-function arryNullAll (arr) {
-  if (!arr) return;
-  arr.forEach(arryNullEl);
-}
-
-function objNullAll (obj) {
-  if (!obj) return;
-  for (var i in obj) obj[i] = null;
-}
-
-function objDestroyAll (obj) {
-  if (!obj) return;
-  for (var i in obj) {
-    if (obj[i]) obj[i].destroy();
-    obj[i] = null;
+  //helper function
+  function canCallMethod(el,methodName){
+    if (!isString(methodName)){
+      throw new Error(methodName + ' is not a string.');
+    }
+    if (!has(el,methodName)){
+      throw new Error(el + ' is not an object or does not have method ' + methodName + '.');
+    }
+    if (!isFunction(el[methodName])){
+      throw new Error('Property destroy of ' + el + ' is not a function.');
+    }
+    return true;
   }
-}
 
-function destructor(item){
-  if (item.destroy) {
-    item.destroy();
+  function arryDestroyEl (el, index, arr) {
+    if (canCallMethod(el,'destroy')){
+      el.destroy();
+    }
+    if (!isArray(arr)){
+      throw new Error(arr + ' is not an array.');
+    }
+    if (!isNumber(index)){
+      throw new Error(index + ' is not a non-negative number.');
+    }
+    if (index < 0){
+      throw new Error(index + ' is not a non-negative number.');
+    }
+    arr[index] = null;
+    return true;
   }
-}
 
-function containerDestroyAll (container) {
-  container.traverse(destructor);
-}
+  function arryNullEl (el, index, arr) {
+    if (!arr) return;
 
-function containerDestroyDeep (container) {
-  containerDestroyAll(container);
-  container.destroy();
-}
+    if (!isArray(arr)){
+      throw new Error(arr + ' is not an array.');
+    }
+    if (!isNumber(index)){
+      throw new Error(index + ' is not a non-negative number.');
+    }
+    if (index < 0){
+      throw new Error(index + ' is not a non-negative number.');
+    }
 
-module.exports = {
-  arryDestroyEl: arryDestroyEl,
-  arryNullEl: arryNullEl,
-  arryDestroyAll : arryDestroyAll,
-  arryNullAll: arryNullAll,
-  objNullAll : objNullAll,
-  objDestroyAll: objDestroyAll,
-  containerDestroyAll: containerDestroyAll,
-  containerDestroyDeep: containerDestroyDeep
+    arr[index] = null;
+    return true;
+  }
+
+  function arryDestroyAll (arr) {
+    if (!arr) return;
+
+    if (!isArray(arr)){
+      throw new Error(arr + ' is not an array.');
+    }
+
+    arr.forEach(arryDestroyEl);
+    return true;
+  }
+
+  function arryNullAll (arr) {
+    if (!arr) return;
+
+    if (!isArray(arr)){
+      throw new Error(arr + ' is not an array.');
+    }
+
+    arr.forEach(arryNullEl);
+    return true;
+  }
+
+  function objNullAll (obj) {
+    if (!obj) return;
+    if (obj === null || 'object' !== typeof obj){
+      throw new Error(obj + ' is not an object.');
+    }
+    for (var i in obj) obj[i] = null;
+    return true;
+  }
+
+  function objDestroyAll (obj) {
+    if (!obj) return;
+    if (obj === null || 'object' !== typeof obj){
+      throw new Error(obj + ' is not an object.');
+    }
+    for (var i in obj) {
+      if (canCallMethod(obj[i],'destroy')){
+        obj[i].destroy();
+      }
+      obj[i] = null;
+    }
+    return true;
+  }
+
+  function destructor(item){
+    if (canCallMethod(item,'destroy')){
+      item.destroy();
+    }
+  }
+
+  function containerDestroyAll (container) {
+    if (canCallMethod(container,'traverse')){
+      container.traverse(destructor);
+    }
+    return true;
+  }
+
+  function containerDestroyDeep (container) {
+    containerDestroyAll(container);
+    if (canCallMethod(container,'destroy')){
+      container.destroy();
+    }
+    return true;
+  }
+
+  return {
+    arryDestroyEl: arryDestroyEl,
+    arryNullEl: arryNullEl,
+    arryDestroyAll : arryDestroyAll,
+    arryNullAll: arryNullAll,
+    objNullAll : objNullAll,
+    objDestroyAll: objDestroyAll,
+    containerDestroyAll: containerDestroyAll,
+    containerDestroyDeep: containerDestroyDeep
+  };
+
 };
+
+module.exports = createLib;
